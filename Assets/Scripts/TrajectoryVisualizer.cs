e6cddeb8fc05de28cace89fb5cd83e3336df4ccc
Joseph Kenny <jckenny59@gmail.com>
2025-08-01 02:44:39 -0400

    add workcell

--- Assets/Scripts/TrajectoryVisualizer.cs
+++ Assets/Scripts/TrajectoryVisualizer.cs
@@ -44,7 +44,8 @@ namespace CompasXR.Robots
         public Dictionary<string, string> URDFRenderComponents = new Dictionary<string, string>();
 
         //List of available robots
-        public List<string> URDFList = new List<string> {"UR3", "UR5", "UR10e", "ETHZurichRFL", "UR20"};
+        public List<string> RobotSelectionList = new List<string> {"UR3", "UR5", "UR10e", "ETHZurichRFL", "UR20", "ECLABBWorkCell"};
+        // public List<string> URDFList = new List<string> {"UR3", "UR5", "UR10e", "ETHZurichRFL", "UR20"};
             
         ////////////////////////////////////////// Monobehaviour Methods ////////////////////////////////////////////////////////
         void Start()
